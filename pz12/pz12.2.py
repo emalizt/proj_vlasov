a57d85877b62e456b8a8653a49fdc0d5eb66d8b5
emalist <vlasovrostik9@gmail.com>
2025-06-20 13:35:27 +0300

    pz12.2.py

--- pz12/pz12.2.py
+++ pz12/pz12.2.py
@@ -1,9 +1,8 @@
 def letter_generator(input_string):
     for char in input_string:
-        if char.isalpha():  # Проверяем, является ли символ буквой
+        if char.isalpha():
             yield char
 
-# Пример использования
 input_str = "Hello, World! 123"
 letters = ''.join(letter_generator(input_str))
 print("Буквы из строки:", letters)
