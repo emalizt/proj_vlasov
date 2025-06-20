e58a13dfe81650f0f2937016413fa94a7b6a0ad6
emalist <vlasovrostik9@gmail.com>
2025-06-20 13:35:08 +0300

    pz12.1.py

--- pz12/pz12.1.py
+++ pz12/pz12.1.py
@@ -2,11 +2,11 @@ def multiply_elements(sequence):
     if len(sequence) < 2:
         raise ValueError("Последовательность должна содержать как минимум два элемента.")
     
-    n = sequence[-1]  # Последний элемент
-    result = [x * n for x in sequence[:-1]]  # Умножаем все элементы до n-1 на n
+    n = sequence[-1]
+    result = [x * n for x in sequence[:-1]]
     return result
 
-# Пример использования
-numbers = [1, 2, 3, 4, 5]  # Пример последовательности
+
+numbers = [1, 2, 3, 4, 5] 
 result = multiply_elements(numbers)
 print("Результат умножения:", result)
