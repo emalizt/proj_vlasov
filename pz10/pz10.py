5e867d1e5b7243c92265f5c2dfd21036d96425f8
emalist <vlasovrostik9@gmail.com>
2025-09-26 15:38:10 +0300

    pz10.py

--- pz10/pz10.py
+++ pz10/pz10.py
@@ -1,15 +1,14 @@
-# Создаем словарь с магазинами и их коллекциями книг
-book_stores = {
-    'Магистр': ['Лермонтов', 'Достоевский', 'Пушкин', 'Тютчев'],
-    'ДомКниги': ['Толстой', 'Грибоедов', 'Чехов', 'Пушкин'],
-    'БукМаркет': ['Пушкин', 'Достоевский', 'Маяковский'],
-    'Галерея': ['Чехов', 'Тютчев', 'Пушкин']
+
+magazines = {
+    "Магистр": {"Лермонтов", "Достоевский", "Пушкин", "Тютчев"},
+    "ДомКниги": {"Толстой", "Грибоедов", "Чехов", "Пушкин"},
+    "БукМаркет": {"Пушкин", "Достоевский", "Маяковский"},
+    "Галерея": {"Чехов", "Тютчев", "Пушкин"}
 }
-
-
-stores_with_mayakovsky = [store for store, books in book_stores.items() 
-                         if 'Маяковский' in books]
-
+magazines_with_mayakovsky = []
+for magazine_name, books in magazines.items():
+    if "Маяковский" in books:
+        magazines_with_mayakovsky.append(magazine_name)
 print("Книги Маяковского можно приобрести в следующих магазинах:")
-for store in stores_with_mayakovsky:
-    print(f"- {store}")
+for magazine in magazines_with_mayakovsky:
+    print(f"- {magazine}")
